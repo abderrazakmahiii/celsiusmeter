9ac66a060c9a5fb1e01900cf78d22371c925c5a7
Abderrazak Mahi <abderrazakmahiii@gmail.com>
2023-03-09 16:38:50 +0000

    adding the api

--- celsiusmeter/src/Components/home.jsx
+++ celsiusmeter/src/Components/home.jsx
@@ -1,43 +1,81 @@
-import React from 'react'
-import {IoMdRainy} from 'react-icons/io';
-import {WiHumidity} from 'react-icons/wi';
-import {FiWind } from 'react-icons/fi';
+import React, { useState } from 'react';
+import { ImLocation2 } from 'react-icons/im';
+import { IoMdRainy } from 'react-icons/io';
+import { WiHumidity } from 'react-icons/wi';
+import { FiWind } from 'react-icons/fi';
+import axios from 'axios';
 
 export default function Home() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState(''); // add initial value here
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=59ab0b19fe2e3a398444b2700bd03e73`;
+  const searchLocation = async (e) => { // make this function asynchronous
+    if (e.key === 'Enter' && location) { // add a check for location
+      try {
+        const response = await axios.get(url);
+        setData(response.data);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
   return (
     <div id="home">
-        <div id="home-text">
-            <h1>We predict the future</h1>
-            <p>Never get caught in the rain again! With CelsiusMeter, you can get up-to-date forecasts for your location and plan your day
-                 with confidence. Our advanced technology provides accurate weather information, so you can stay ahead of the storm. Whether 
-                 you're planning a picnic, going for a run, or heading out on a road trip, CelsiusMeter has you covered."</p>
+      <div id="home-text">
+        <h1>We predict the future</h1>
+        <p>
+          Never get caught in the rain again! With CelsiusMeter, you can get
+          up-to-date forecasts for your location and plan your day with
+          confidence. Our advanced technology provides accurate weather
+          information, so you can stay ahead of the storm. Whether you're
+          planning a picnic, going for a run, or heading out on a road trip,
+          CelsiusMeter has you covered."
+        </p>
+      </div>
+      <div id="weather">
+        <div id="right">
+          <div id="location">
+            <ImLocation2 className="icon" />
+            <input
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+              onKeyPress={searchLocation}
+              placeholder="New York"
+            />
+          </div>
         </div>
-        <div id="weather">
-            <div id="top">
-                <div className="left">
-                    <h3>New York</h3>
-                    <h1>34°C</h1>
-                    <p>Feels like 30°C</p>
-                </div>
-                <div className="right">
-                    Sunny
-                </div>
-            </div>
-            <div id="bottom">
-                <div className='element'>
-                    <h4><IoMdRainy /> Precipitation</h4>
-                    <span>10%</span>
-                </div>
-                <div className='element'>
-                    <h4><WiHumidity /> Humidity</h4>
-                    <span>10%</span>
-                </div>
-                <div className='element'>
-                    <h4><FiWind /> Precipitation</h4>
-                    <span>10%</span>
-                </div>
-            </div>
+        <div id="top">
+          <div className="left">
+            <h3>{data.name}</h3> {/* remove the curly braces */}
+            <h1>{data.main && data.main.temp.toFixed()+'°C'}</h1> {/* add a check for data.main */}
+            <p>{data.main && `Feels like ${data.main.feels_like.toFixed()}°C`}</p> {/* add a check for data.main */}
+          </div>
+          <div className="right">
+            {data.weather && data.weather[0].description} {/* add a check for data.weather */}
+          </div>
         </div>
+        <div id="bottom">
+          <div className="element">
+            <span>{data.main && data.main.pressure}</span> {/* add a check for data.main */}
+            <h4>
+              <IoMdRainy /> Pressure
+            </h4>
+          </div>
+          <div className="element">
+            <span>{data.main && `${data.main.humidity}%`}</span> {/* add a check for data.main */}
+            <h4>
+              <WiHumidity /> Humidity
+            </h4>
+          </div>
+          <div className="element">
+            <span>{data.wind && `${data.wind.speed} MPH`}</span> {/* add a check for data.wind */}
+            <h4>
+              <FiWind /> Winds
+            </h4>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- celsiusmeter/src/Components/navbar.jsx
+++ celsiusmeter/src/Components/navbar.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
-import { ImLocation2 } from 'react-icons/im';
 import { TiWeatherPartlySunny } from 'react-icons/ti';
 
-export default function navbar() {
+export default function Navbar() {
+
   return (
     <div id='navbar'>
         <div id="left">
         <a href=''>CelsiusMeter <TiWeatherPartlySunny className='icon' /></a>
         </div>
-        <div id="right">
-            <p>8th March 2023</p>
-            <div id="location">
-                <ImLocation2 className='icon' />
-                <input placeholder=' New York' />
-            </div>
-        </div>
     </div>
   )
-}
+  }
